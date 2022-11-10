6156d47d50c0b8ed30e26264b18fcd589bacc15b
Abdul Jamil Raoufi <abdul.raoufi@tekschool.us>
2022-11-09 20:14:32 -0500

    in review session branch

--- src/gitReview/GitReviewPractice.java
+++ src/gitReview/GitReviewPractice.java
@@ -1,5 +1,13 @@
 package gitReview;
 
 public class GitReviewPractice {
+	public static void main(String[] args) {
+		
+		
+	}
 
+	
+	public void test () {
+		System.out.println("this method is in reviewSession branch");
+	}
 }
